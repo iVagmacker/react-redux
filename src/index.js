1fa1f84ad3527cd4d9aa01d94fa30bfe68b0702b
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-06-05 20:36:50 -0300

    Utilizando jsx e trabalhando funções com parametros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
+import First from './components/basic/First'
+import WithParameter from './components/basic/WithParameter'
 
-ReactDOM.render('Olá React', document.getElementById('root'))
+ReactDOM.render(
+  <div id="app">
+    <First></First>
+    <WithParameter
+      titulo="Situação do Aluno"
+      subtitulo="Pedro Silva"
+      nota={9.3} />
+  </div>,
+  document.getElementById('root'))
